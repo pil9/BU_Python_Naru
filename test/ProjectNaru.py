de179dc4ec247047e3fae7b09d6b50f47d36db28
pil9 <ljp3326@naver.com>
2021-11-28 21:52:39 +0900

    코드수정

--- test/ProjectNaru.py
+++ test/ProjectNaru.py
@@ -113,6 +113,8 @@ class OptionWindow3(QMainWindow):
         index=engine.recommand_analyzing()
         result=engine.lookup_db_data(index)
 
+        for line in result.items():
+            print(line)
         #Pixmap사용
         self.qPixmapFileVar = QPixmap()
 
